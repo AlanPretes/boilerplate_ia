03331ccd1367ee88c9fa62f7fb5f35cd3a990b97
Alan Gabriel Pretes <96142149+AlanPretes@users.noreply.github.com>
2024-09-12 17:14:27 -0300

    :fix; test settings media url

--- config/settings.py
+++ config/settings.py
@@ -147,8 +147,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/boilerplate_ia/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'boilerplate_ia/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
