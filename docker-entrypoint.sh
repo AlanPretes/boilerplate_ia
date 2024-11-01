e7f61908d107637441eb94051ccde1279339ec1b
Nathan <nathan@autovist.com.br>
2024-11-01 14:41:10 -0300

    fix: ajusta docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,11 +11,12 @@ echo "Running migrate..."
 python manage.py migrate || { echo 'Migration failed' ; exit 1; }
 
 echo "Starting server..."
-gunicorn \
-    --workers 6 \
-    --threads 8 \
-    --max-requests 80000 \
-    --timeout 120 \
-    --keep-alive 5 \
-    config.wsgi:application \
-    --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
+# gunicorn \
+#     --workers 6 \
+#     --threads 8 \
+#     --max-requests 80000 \
+#     --timeout 120 \
+#     --keep-alive 5 \
+#     config.wsgi:application \
+#     --bind 0.0.0.0:8000
